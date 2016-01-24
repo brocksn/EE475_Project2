3d75feec72d0b1861cc7b86b5091e99cf96f5c93
brocksn <brocksn@gmail.com>
2016-01-23 17:36:21 -0800

    Update user.c

--- user.c
+++ user.c
@@ -23,18 +23,4 @@
 /* User Functions                                                             */
 /******************************************************************************/
 
-/* <Initialize variables in user.h and insert code for user algorithms.> */
-
-void InitApp(void)
-{
-    /* TODO Initialize User Ports/Peripherals/Project here */
-
-    /* Setup analog functionality and port direction */
-
-    /* Initialize peripherals */
-
-    /* Configure the IPEN bit (1=on) in RCON to turn on/off int priorities */
-
-    /* Enable interrupts */
-}
 
