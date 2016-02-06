e56a099dd8a4827c071530fb321dbd2e1f86c645
brocksn <brocksn@gmail.com>
2016-02-05 17:10:05 -0800

    Update system.c

--- system.c
+++ system.c
@@ -22,50 +22,67 @@
 
 #define _XTAL_FREQ 20000000
 
+char buf;
+
 
 
 void read() {
-    while (!SSPSTATbits.BF);
-    read_slave_address();
-    read_mode();
-    read_duration();
-    read_historical();
+    buf = SSPBUF;
+    SSPBUF = 0x00;
+    
+    
+    LATD = buf;
+    read_slave_address(buf);
+    read_mode(buf);
+    read_duration(buf);
+    read_historical(buf);
+    /*
+    if (buf == 0x61) {
+        SSPBUF = 0xFF;
+    } else {
+        SSPBUF = 0xAA;
+    }
+    */
 }
 
-void read_slave_address() {
-    slave_address = (SSPBUF >> 5) & 0x7;
+void read_slave_address(char buf) {
+    slave_address = (buf >> 5) & 0x7;
 }
 
-void read_mode() {
-    mode = SSPBUF & 0x7;
+void read_mode(char buf) {
+    mode = buf & 0x7;
 }
 
-void read_duration() {
-    window = (SSPBUF >> 4) & 0x1;
+void read_duration(char buf) {
+    window = (buf >> 4) & 0x1;
 }
 
-void read_historical() {
-    hist = (SSPBUF >> 3) & 0x1;
+void read_historical(char buf) {
+    hist = (buf >> 3) & 0x1;
 }
 
 void transmit_data() {
-    unsigned long data = (unsigned long) slave_address;
-    data += ((unsigned long) window) * 10 ;
-    data += ((unsigned long) mode) * 100;  
-    char dump;
-    if (SSPSTATbits.BF) {
-        dump = SSPBUF;
+    
+    unsigned long data = (unsigned long) mode;
+    data += ((unsigned long) slave_address) * 10 ;
+    data += ((unsigned long) window) * 100;
+    //char dump;
+    //if (SSPSTATbits.BF) {
+    //    dump = SSPBUF;
+    //}
+    if (buf > 0xD5) {
+        SSPBUF = 0xFF;
+    } else {
+        SSPBUF = 0xFF & data;
+        SSPCON1bits.WCOL = 1;
+        if (hist) {
+            int i;
+            for (i=0; i < 15; i++) {
+                data += 1;
+                while (SSPCON1bits.WCOL);
+                SSPBUF = 0xFF & data;
+                SSPCON1bits.WCOL = 1;
+            }
+        }
     }
-    SSPBUF = 0xFF & data;
-    SSPCON1bits.WCOL = 0x1;
-    while (SSPCON1bits.WCOL);
-    SSPBUF = (data << 8) & 0xFF;
-    SSPCON1bits.WCOL = 0x1;
-    while (SSPCON1bits.WCOL);
-    SSPBUF = (data << 16) & 0xFF;
-    SSPCON1bits.WCOL = 0x1;
-    while (SSPCON1bits.WCOL);
-    SSPBUF = (data << 24) & 0xFF;
-    SSPCON1bits.WCOL = 0x1;
-    while (SSPCON1bits.WCOL);
 }
